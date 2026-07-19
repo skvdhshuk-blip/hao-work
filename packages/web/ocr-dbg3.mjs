cffe8635e73871c8d0e81351d7284bead847a689
wanghao <wanghao@shiyanjia.com>
2026-07-19 14:15:31 +0800

    chore: remove OCR debug leftovers [skip ci]

--- packages/web/ocr-dbg3.mjs
+++
@@ -1,5 +0,0 @@
-import { createImageConverters } from './server/lib/haocode/image-converters.js';
-import fs from 'node:fs';
-const c = createImageConverters({ dataDir: '/tmp/ocr-dbg-cache', logger: { log(){}, error(){} } });
-const dataUri = 'data:image/png;base64,' + fs.readFileSync('/tmp/ocr-big.png').toString('base64');
-console.log('OCR:', JSON.stringify(await c.ocr(dataUri)));
--- packages/web/ocr-dbg4.mjs
+++
@@ -1,6 +0,0 @@
-import { createWorker } from 'tesseract.js';
-const dir = process.argv[2];
-const w = await createWorker(['eng'], 1, { cachePath: dir, workerBlobURL: false });
-const r = await w.recognize(process.argv[3]);
-console.log('OCR:', JSON.stringify(r.data.text));
-await w.terminate();
